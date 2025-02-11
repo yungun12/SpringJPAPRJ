53448508ad03ab422505ad55bfc11704ba5e928d
윤건 <yungun200512@gmail.com>
2025-02-11 15:39:10 +0900

    commit

---
+++ src/main/java/kopo/poly/SpringJpaprjApplication.java
@@ -0,0 +1,13 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaprjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaprjApplication.class, args);
+    }
+
+}
